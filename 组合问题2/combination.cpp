cee5528fff2bf0ece47f5fed902e0abc3f2206af
赵政 <www.a821400729@qq.com>
2017-12-02 16:26:29 +0800

    add

---
+++ 组合问题2/combination.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+void comb(char * arr,int length,int index,int k,vector<char>& cur ,vector<vector<char>>& res)
+{
+	//减枝条件1
+	if(index>length){
+		return ;
+	}
+	//减枝条件2
+	if(cur.size()>k){
+		return ;
+	}
+	if(index==length){
+		if(cur.size()==k){
+			res.push_back(cur);
+		}
+	}
+	//进入下一波递归
+	//取
+	cur.push_back(arr[index]);
+	comb(arr,length,index+1,k,cur,res);
+	//不取
+	cur.pop_back();
+	comb(arr,length,index+1,k,cur,res);
+}
+
+
+int main(){
+	char * c_arr="abcd";
+	vector<char> cur;
+	vector<vector<char>> res;
+	comb(c_arr,4,0,3,cur,res);
+
+	return 0;
+}
---
+++ 组合问题2/readme.md
@@ -0,0 +1,16 @@
+# Combinations
+
+Given two integers n and k, return all possible combinations of k numbers out of 1 ... n.
+For example,
+
+```
+arr=[1,2,3,4] , k=2
+[
+  [2,4],
+  [3,4],
+  [2,3],
+  [1,2],
+  [1,3],
+  [1,4],
+]
+```
