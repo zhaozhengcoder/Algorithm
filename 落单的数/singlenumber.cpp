05a8196bb2bc951082d2401bd0dd728921b6ffde
赵政 <www.a821400729@qq.com>
2017-12-12 22:34:33 +0800

    add

--- 落单的数/singlenumber.cpp
+++ 落单的数/singlenumber.cpp
@@ -29,3 +29,19 @@ public:
 		return *it;
 	}
 };
+/*
+利用异或^运算，两个相同的数的异或的结果是0，就这样遍历之后的结果就是那个数
+class Solution:
+    """
+    @param A : an integer array
+    @return : a integer
+    """
+    def singleNumber(self, A):
+        # write your code here
+        if A.length ==0:
+            return 0 
+        n = A[0]
+        for i in range(1,len(A)):
+            n = n ^ A[i]
+        return n 
+*/
