7c58e38aaddb41a4ef385a3472435d49240e9dd2
赵政 <www.a821400729@qq.com>
2017-12-16 19:38:22 +0800

    update

---
+++ ---搜索二维矩阵---/礼物的最大价值/递归+缓存的本版.cpp
@@ -0,0 +1,130 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+	//递归
+	int calu_maxpath(vector<vector<int>> matrix,int index_i,int index_j){
+		if(index_i==0||index_j==0){
+			if(index_i==0 && index_j!=0){
+				int sum=0;
+				for(int j=0;j<=index_j;j++){
+					sum+=matrix[0][j];
+				}
+				return sum;
+			}
+			else if(index_i!=0 && index_j==0){
+				int sum=0;
+				for(int i=0;i<=index_i;i++){
+					sum+=matrix[i][0];
+				}
+				return sum;
+			}
+			else {
+				//index-i==0 && index-j==0
+				return matrix[0][0];
+			}
+		}
+		else{
+			int res1=calu_maxpath(matrix,index_i-1,index_j);
+			int res2=calu_maxpath(matrix,index_i,index_j-1);
+			int ret=max(res1,res2)+matrix[index_i][index_j];
+			return ret;
+		}
+	}
+
+	//递归版本
+	int findmaxgiftpath_recursion(vector<vector<int>> matrix){
+		int ret=calu_maxpath(matrix,matrix[0].size()-1,matrix.size()-1);
+		return ret;
+	}
+};
+
+class Solution2 {
+	public:
+	int calu_maxpath(vector<vector<int>> matrix,vector<vector<int>> &dp,int index_i,int index_j){
+		//查询缓存
+		if(dp[index_i][index_j]!=-1){
+			return dp[index_i][index_j];
+		}
+
+		if(index_i==0||index_j==0){
+			if(index_i==0 && index_j!=0){
+				int sum=0;
+				for(int j=0;j<=index_j;j++){
+					sum+=matrix[0][j];
+				}
+				dp[index_i][index_j]=sum;
+				return sum;
+			}
+			else if(index_i!=0 && index_j==0){
+				int sum=0;
+				for(int i=0;i<=index_i;i++){
+					sum+=matrix[i][0];
+				}
+				dp[index_i][index_j]=sum;
+				return sum;
+			}
+			else {
+				//index-i==0 && index-j==0
+				dp[index_i][index_j]= matrix[0][0];
+				return matrix[0][0];
+			}
+		}
+		else{
+			int res1=calu_maxpath(matrix,dp,index_i-1,index_j);
+			int res2=calu_maxpath(matrix,dp,index_i,index_j-1);
+			int ret=max(res1,res2)+matrix[index_i][index_j];
+
+			dp[index_i][index_j]=ret;
+			return ret;
+		}
+	}
+
+	int findmaxgiftpath(vector<vector<int>> matrix){
+		vector<vector<int>> dp;
+		//init
+		int arr[]={-1,-1,-1,-1};
+		vector<int> v1(arr,arr+4);
+		vector<int> v2(arr,arr+4);
+		vector<int> v3(arr,arr+4);
+		vector<int> v4(arr,arr+4);
+		dp.push_back(v1);
+		dp.push_back(v2);
+		dp.push_back(v3);
+		dp.push_back(v4);
+
+
+		int ret=calu_maxpath(matrix,dp,matrix[0].size()-1,matrix.size()-1);
+		return ret;
+	}
+};
+
+
+int main(){
+	int n1[] = {1, 10, 3, 8} ;
+	vector<int> v1(n1,n1+4);
+	int n2[] = {12, 2,9,6} ;
+	vector<int> v2(n2,n2+4);
+
+	int n3[] = {5,7,4,11} ;
+	vector<int> v3(n3,n3+4);
+
+	int n4[] = {3,7,16,5} ;
+	vector<int> v4(n4,n4+4);
+
+	vector<vector<int> > matrix;
+	matrix.push_back(v1);
+	matrix.push_back(v2);
+	matrix.push_back(v3);
+	matrix.push_back(v4);
+
+
+
+	Solution2 s;
+	cout<<s.findmaxgiftpath(matrix);
+
+
+	return 0;
+}
