cf2a16e7f031e1126c8125a5f51ca0bebd64234a
赵政 <www.a821400729@qq.com>
2018-04-06 11:30:26 +0800

    add 装水最多的容器

---
+++ 装水最多的容器/solution.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<algorithm> 
+#include<vector>
+using namespace std;
+
+class Solution2 {
+public:
+	int maxArea(vector<int> height) {
+		int left = 0;
+		int right = height.size() - 1;
+		int maxares = (right - left) * min(height[left], height[right]);
+
+		while (left < right) {
+			if (height[left] < height[right]) {
+				left++;
+			}
+			else {
+				right--;
+			}
+			int ares = (right - left) * min(height[left], height[right]);
+			maxares = max(maxares, ares);
+		}
+		return maxares;
+	}
+};
+
+int main(int argc, char const *argv[])
+{
+	vector<int> vi = { 1,2,3 };
+	Solution2 s;
+
+	cout<<s.maxArea(vi);
+
+	system("pause");
+	return 0;
+}
