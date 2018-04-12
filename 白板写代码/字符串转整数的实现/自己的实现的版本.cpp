aefabe2ed53505f908a37193faf65e739da83d76
赵政 <www.a821400729@qq.com>
2018-04-12 16:25:41 +0800

    edited

--- 白板写代码/字符串转整数的实现/自己的实现的版本.cpp
+++ 白板写代码/字符串转整数的实现/自己的实现的版本.cpp
@@ -1,37 +1,46 @@
-#include<stack>
-#include<iostream>
-#include<deque>
-#include<vector>
-#include<string>
-#include <sstream>
-#include <assert.h>
+#include <iostream>
+#include <vector>
+
 using namespace std;
 
-int my_atoi(char * c) {
-	assert(*c >= '0' && *c <= '9');
-	int diff = (*c) - '0';
-	return diff + 0;
-}
-
-int string_to_int(string str) {
-	int count = 0;
-	int base = 10;
-	int sum = 0;
-	for (int i = str.size() - 1; i >= 0; i--) {
-		char temp = str[i];
-		int ret = my_atoi(&temp);
-		sum += ret * pow(base, count);
-		count++;
+int myatoi(char * c) {
+	//先过滤空格
+	while (*c == ' ') {
+		c++;
 	}
-	return sum;
+
+	int sign = 1;
+	if (*c == '+' || *c == '-') {
+		if (*c == '-') {
+			sign = -1;
+		}
+		c++;
+	}
+
+	int val = 0;
+	while (*c != '\0' && (*c >= '0' && *c <= '9')) {
+		int offset = *c - '0';
+		val = val * 10 + offset;
+		c++;
+	}
+	return  val * sign;
+
 }
 
-int main() {
-	string str = "123";
-	cout<<string_to_int(str);
-	
 
+int main()
+{
 	
+	char s1[] = "123456";
+	char s2[] = "+123";
+	char s3[] = "-123";
+	char s4[] = "   +123    ";
+
+	cout << myatoi(s1) << endl;
+	cout << myatoi(s2) << endl;
+	cout << myatoi(s3) << endl;
+	cout << myatoi(s4) << endl;
+
 	system("pause");
 	return 0;
 }
